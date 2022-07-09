137ae59ee394251890e3305acbafcdf1fdb7653e
Dmitry <ddmitiy@mail.ru>
2022-07-09 20:57:36 +0300

    Add View for Register

--- short_link/accounts/views.py
+++ short_link/accounts/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-from accounts.forms import AuthUserForm
+from accounts.forms import AuthUserForm, RegisterUserForm
 # Create your views here.
+from django.views.generic import CreateView
+
+
 def index(request):
     return render(request,"index.html")
 
@@ -12,3 +16,11 @@ class AuthUserLogin(LoginView):
     template_name = "auth/login.html"
     form_class = AuthUserForm
 
+class UserRegister(CreateView):
+    """
+   View for creating User.
+    """
+    model = User
+    template_name = "register.html"
+    form_class = RegisterUserForm
+    success_msg = "Пользователь создан"
