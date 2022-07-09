c9c8de40de0cc9423b00dc60b4d691e78691ff45
Dmitry <ddmitiy@mail.ru>
2022-07-09 20:13:28 +0300

    Correct login for User

--- short_link/accounts/urls.py
+++ short_link/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from accounts.views import AuthUserLogin, index
 
 urlpatterns = [
-    path('',index),
+    path('',index, name= 'home'),
     path('login/', AuthUserLogin.as_view(), name="login"),
 ]
