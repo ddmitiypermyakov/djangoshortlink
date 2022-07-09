85eccb3f213a811dece64525685891a36cbb5f8c
Dmitry <ddmitiy@mail.ru>
2022-07-09 20:56:52 +0300

    Add url for register

--- short_link/accounts/urls.py
+++ short_link/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import AuthUserLogin, index
+from accounts.views import AuthUserLogin, UserRegister, index
 
 urlpatterns = [
     path('',index, name= 'home'),
     path('login/', AuthUserLogin.as_view(), name="login"),
+    path('register/', UserRegister.as_view(), name="register"),
 ]
