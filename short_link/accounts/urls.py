40478314fe12e1a79300a1ceaefa529de34fef55
Dmitry <ddmitiy@mail.ru>
2022-07-10 01:21:28 +0300

    Add url logout

--- short_link/accounts/urls.py
+++ short_link/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from accounts.views import AuthUserLogin, UserRegister, index
+from accounts.views import AuthUserLogin, AuthUserLogout, UserRegister, index
 
 urlpatterns = [
     path('',index, name= 'home'),
     path('login/', AuthUserLogin.as_view(), name="login"),
+    path('logout/', AuthUserLogout.as_view(), name="logout"),
     path('register/', UserRegister.as_view(), name="register"),
 ]
