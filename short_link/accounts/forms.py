d42c4bdad937a837e93bd7ee00fb19c61f1d51e7
Dmitry <ddmitiy@mail.ru>
2022-07-09 18:30:43 +0300

    Correct name form

--- short_link/accounts/forms.py
+++ short_link/accounts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
 
-class AuthUserLogin(AuthenticationForm, forms.ModelForm):
+class AuthUserForm(AuthenticationForm, forms.ModelForm):
     class Meta:
         model = User
         fields = ("username", "password",)
