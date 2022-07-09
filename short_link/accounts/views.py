3d414d70fd52e719966fadfa5cb795a6c623a848
Dmitry <ddmitiy@mail.ru>
2022-07-09 18:06:19 +0300

    Add Login View

--- short_link/accounts/views.py
+++ short_link/accounts/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-
+from accounts.forms import AuthUserForm
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+class AuthUserLogin(LoginView):
+    tamplate_name = "auth/login.html"
+    form_class = AuthUserForm
+
