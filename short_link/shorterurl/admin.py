5192b3d1cd52be546caa670c77b10e3b833f92c1
Dmitry <ddmitiy@mail.ru>
2022-07-10 03:00:35 +0300

    Create model short link

--- short_link/shorterurl/admin.py
+++ short_link/shorterurl/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from shorterurl.models import Short_link
 # Register your models here.
+admin.site.register(Short_link)
--- short_link/shorterurl/models.py
+++ short_link/shorterurl/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from shorterurl.converter_url import create_short_url
 
-# Create your models here.
+
+class Short_link(models.Model):
+    """
+    Creates a short url based on the long one
+    """
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    long_url = models.URLField(verbose_name="Длинная ссылка")
+    short_url = models.CharField(max_length=15, unique=True, blank=True, verbose_name="Короткая ссылка")
+    user_use = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f'{self.long_url} to {self.short_url}'
+
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = create_short_url(self)
+
+        super().save(*args, **kwargs)
