74a085ef4b4c954551a18369eed62b4ccb32eca1
Dmitry <ddmitiy@mail.ru>
2022-07-10 16:09:09 +0300

    Correcr project template and static

--- short_link/short_link/settings.py
+++ short_link/short_link/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'short_link.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__), '../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
