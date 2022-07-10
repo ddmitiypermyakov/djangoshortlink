72fd77786a44b4387acb7b8d26e0cbef96f2fc02
Dmitry <ddmitiy@mail.ru>
2022-07-10 21:41:24 +0300

    Correct all files

--- shorterurl/models.py
+++ shorterurl/models.py
@@ -8,7 +8,7 @@ class Short_link(models.Model):
     Creates a short url based on the long one
     """
     created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    long_url = models.URLField(verbose_name="Длинная ссылка")
+    long_url = models.URLField(max_length=500,verbose_name="Длинная ссылка")
     short_url = models.CharField(max_length=15, unique=True, blank=True, verbose_name="Короткая ссылка")
     user_use = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
 
